b457ca563a046c4694b724eee2fe01c758c0d1fb
Becker <bckfl10@gmail.com>
2019-04-01 00:15:54 +0200

    First commit

--- QATest/UnitTest1.cs
+++ QATest/UnitTest1.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
-
+using OpenQA.Selenium;
+using OpenQA.Selenium.Firefox;
+using OpenQA.Selenium.Chrome;
 namespace QATest
 {
     [TestClass]
@@ -9,6 +11,23 @@ namespace QATest
         [TestMethod]
         public void TestMethod1()
         {
+           
+            IWebDriver driver2 = new ChromeDriver();
+            driver2.Url = "http://localhost:54507/Factura/Create";
+            IWebElement IdElement = driver2.FindElement(By.Id("IdFactura"));
+            IdElement.SendKeys("1");
+            IWebElement DescripcionElement = driver2.FindElement(By.Id("Descripcion"));
+            DescripcionElement.SendKeys("Compra total");
+            IWebElement IvaElement = driver2.FindElement(By.Id("Iva"));
+            IvaElement.SendKeys("21%");
+            IWebElement PrecioElement = driver2.FindElement(By.Id("Precio"));
+            PrecioElement.SendKeys("50");
+
+            PrecioElement.Submit();
+
+
+
+
         }
     }
 }
---
+++ WebApp/Models/Factura.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApp.Models
+{
+    public class Factura
+    {
+        public int IdFactura{get;set;}
+        public string Descripcion { get; set; }
+        public int Iva { get; set; }
+        public float Precio { get; set; }
+
+}
+}
